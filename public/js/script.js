f5f87e8574ba4453809eb41790d0fc7631704ffa
alejandro <alejandro.garcia@dsai-soluciones.com.mx>
2020-03-09 02:47:19 -0600

    modificaciones en datatables

--- public/js/script.js
+++ public/js/script.js
@@ -26,29 +26,76 @@ $(document).ready(function() {
           "oAria": {
               "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
               "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-          }
-        },
+          },
+            "buttons": {
+                "copyTitle": 'Informacion copiada',
+                "copyKeys": 'Use your keyboard or menu to select the copy command',
+                "copySuccess": {
+                    "_": '%d filas copiadas al portapapeles',
+                    "1": '1 fila copiada al portapapeles'
+                },
 
-        dom: 'Bfrtip',
-        buttons:[
-			{
-				extend:    'excelHtml5',
-				text:      '<i class="fa fa-file-excel-o"></i> ',
-				titleAttr: 'Exportar a Excel',
-				className: 'btn btn-success'
-			},
-			{
-				extend:    'pdfHtml5',
-				text:      '<i class="fa fa-file-pdf-o"></i> ',
-				titleAttr: 'Exportar a PDF',
-				className: 'btn btn-danger'
-			},
-			{
-				extend:    'csvHtml5',
-				text:      '<i class="fa fa-print"></i> ',
-				titleAttr: 'CSV',
-				className: 'btn btn-primary'
-			},
-		]
+                "pageLength": {
+                    "_": "Mostrar %d filas",
+                    "-1": "Mostrar Todo"
+                }
+            }
+        },
+        responsive:'true',
+        "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "Mostrar Todo"]],
+        dom: "<'row'<'col-sm-12 col-md-6'B><'col-sm-12 col-md-6'f>> <'row'<'col-sm-12'tr>> <'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+        buttons: {
+          dom: {
+            container: {
+                tag: 'div',
+                className: 'flexcontent'
+            },
+            buttonLiner: {
+                tag: null
+            }
+          },
+          buttons:[
+      			{
+      				extend:    'excelHtml5',
+      				text:      '<i class="fa fa-file-excel-o"></i> ',
+      				titleAttr: 'Exportar a Excel',
+      				className: 'btn btn-success'
+      			},
+      			{
+      				extend:    'pdfHtml5',
+      				text:      '<i class="fa fa-file-pdf-o"></i> ',
+      				titleAttr: 'Exportar a PDF',
+      				className: 'btn btn-danger',
+              customize:function(doc) {
+                doc.styles.title = {
+                    color: '#004fff',
+                    fontSize: '15',
+                    alignment: 'center'
+                }
+                doc.styles['td:nth-child(2)'] = {
+                    width: '100px',
+                    'max-width': '100px'
+                },
+                doc.styles.tableHeader = {
+                    fillColor:'#0094ff',
+                    color:'white',
+                    alignment:'center'
+                },
+                doc.content[1].margin = [ 100, 0, 100, 0 ]
+              }
+      			},
+            {
+              extend:    'csvHtml5',
+              text:      '<i class="fa fa-print"></i> ',
+              titleAttr: 'CSV',
+              className: 'btn btn-primary'
+            },
+            {
+              extend: 'pageLength',
+              titleAttr: 'Registros a mostrar',
+              className: 'selectTable'
+            }
+    		]
+      }
     });
 });
